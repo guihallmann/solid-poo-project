474139ef69e59c75f0cabb839ea42445adcb8be7
Guilherme <guilhermehallmann@gmail.com>
2022-06-27 18:33:28 -0300

    feat: creating Warrior and Ranger classes and importing archetypes to index

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,18 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Ranger extends Archetype {
+  private _energyType: EnergyType = 'stamina';
+  static instances = 0;
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    this.instances += 1;
+    return this.instances;
+  }
+}
+
+export default Ranger;
